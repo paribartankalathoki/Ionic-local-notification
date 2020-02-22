c51c004948bf063c862a48d7d0c699e4623d8d37
paribartan <paribartankalathoki@gmail.com>
2020-02-22 13:39:09 +0545

    added local notification

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { LocalNotifications} from '@ionic-native/local-notifications/ngx'
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -20,6 +22,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    LocalNotifications,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 
 @Component({
   selector: 'app-folder',
@@ -9,10 +10,25 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
   public folder: string;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(private activatedRoute: ActivatedRoute, private localNotifications: LocalNotifications) { }
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
+  simpleNotif() {
+    console.log('cliecke');    
+    this.localNotifications.schedule({
+      id: 1,
+      title: 'Notification',
+      foreground: true,
+      lockscreen: true,
+      vibrate: true,
+      wakeup: true,
+      text: 'Local Notification for testing',
+      icon:'../../assets/icon/favicon.png',
+      data: { secret: 'secret' }
+    });
   }
 
 }
